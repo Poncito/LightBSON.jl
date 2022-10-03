c9cbf8c441ef57d035fb22d6902d5a886521d1ec
Romain Poncet <romain.pct@gmail.com>
2022-10-03 23:22:46 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,16 +12,16 @@ using UnsafeArrays
 struct EmptyStruct end
 
 @testset "LightBSON.jl" begin
-    # include("reader_tests.jl")
-    # include("index_tests.jl")
-    # include("indexed_reader_tests.jl")
-    # include("write_buffer_tests.jl")
-    # include("writer_tests.jl")
-    # include("dict_tests.jl")
-    # include("struct_tests.jl")
-    # include("corpus_tests.jl")
-    # include("object_id_tests.jl")
-    # include("convenience_tests.jl")
-    # include("representation_tests.jl")
+    include("reader_tests.jl")
+    include("index_tests.jl")
+    include("indexed_reader_tests.jl")
+    include("write_buffer_tests.jl")
+    include("writer_tests.jl")
+    include("dict_tests.jl")
+    include("struct_tests.jl")
+    include("corpus_tests.jl")
+    include("object_id_tests.jl")
+    include("convenience_tests.jl")
+    include("representation_tests.jl")
     include("schema_tests.jl")
 end
